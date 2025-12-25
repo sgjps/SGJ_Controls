f61258e72b520c584bfa1c4fcf5f897d5a4c5b30
Grzegorz <66561112+sgjps@users.noreply.github.com>
2025-12-25 19:44:47 +0100

    Packages Lazarus and Delphi

---
+++ Package_Lazarus/sgj_controls.pas
@@ -0,0 +1,29 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit SGJ_Controls;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  SGJ.LinkLabel, SGJ.Button, SGJ.SimpleButton, SGJ.CheckBox, SGJ.Panel, 
+  SGJ.ListView, SGJ.ShellTreeView, SGJListViewPropEdit, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+  RegisterUnit('SGJ.LinkLabel', @SGJ.LinkLabel.Register);
+  RegisterUnit('SGJ.Button', @SGJ.Button.Register);
+  RegisterUnit('SGJ.SimpleButton', @SGJ.SimpleButton.Register);
+  RegisterUnit('SGJ.CheckBox', @SGJ.CheckBox.Register);
+  RegisterUnit('SGJ.Panel', @SGJ.Panel.Register);
+  RegisterUnit('SGJ.ListView', @SGJ.ListView.Register);
+  RegisterUnit('SGJ.ShellTreeView', @SGJ.ShellTreeView.Register);
+end;
+
+initialization
+  RegisterPackage('SGJ_Controls', @Register);
+end.
