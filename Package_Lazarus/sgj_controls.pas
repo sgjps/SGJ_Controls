27fc0650add85c869314d7daf5f5776560870e9d
Grzegorz <66561112+sgjps@users.noreply.github.com>
2026-02-08 12:53:18 +0100

    Add files via upload

--- Package_Lazarus/sgj_controls.pas
+++ Package_Lazarus/sgj_controls.pas
@@ -9,7 +9,8 @@ interface
 
 uses
   SGJ.LinkLabel, SGJ.Button, SGJ.SimpleButton, SGJ.CheckBox, SGJ.Panel, 
-  SGJ.ListView, SGJ.ShellTreeView, SGJListViewPropEdit, SGJ.Win32PopupMenu, 
+  SGJ.ShellTreeView, SGJ.ListView, SGJListViewPropEdit, SGJ.Win32PopupMenu, 
+  SGJ.Win32Extension, SGJ.TitleBarCtrls, SGJ.CalendarView, SGJ.ExpandPanel, 
   LazarusPackageIntf;
 
 implementation
@@ -21,9 +22,13 @@ begin
   RegisterUnit('SGJ.SimpleButton', @SGJ.SimpleButton.Register);
   RegisterUnit('SGJ.CheckBox', @SGJ.CheckBox.Register);
   RegisterUnit('SGJ.Panel', @SGJ.Panel.Register);
-  RegisterUnit('SGJ.ListView', @SGJ.ListView.Register);
   RegisterUnit('SGJ.ShellTreeView', @SGJ.ShellTreeView.Register);
+  RegisterUnit('SGJ.ListView', @SGJ.ListView.Register);
   RegisterUnit('SGJ.Win32PopupMenu', @SGJ.Win32PopupMenu.Register);
+  RegisterUnit('SGJ.Win32Extension', @SGJ.Win32Extension.Register);
+  RegisterUnit('SGJ.TitleBarCtrls', @SGJ.TitleBarCtrls.Register);
+  RegisterUnit('SGJ.CalendarView', @SGJ.CalendarView.Register);
+  RegisterUnit('SGJ.ExpandPanel', @SGJ.ExpandPanel.Register);
 end;
 
 initialization
